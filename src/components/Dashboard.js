9180c25d1ed534ef7d3817064aad79451c4fe7fd
Alex Kemboi <akemboi@KWFTBANK.INT>
2023-04-28 17:38:56 +0300

    changed appearance

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,18 +1,59 @@
-import './styles/DashboardStyles.css'
+import React from 'react';
+import ApexCharts from 'apexcharts';
+import './styles/DashboardStyles.css';
+
 function Dashboard() {
+  var options = {
+    series: [{
+      name: "Success",
+      data: [80]
+    },
+    {
+      name: "Failure",
+      data: [20]
+    }],
+    chart: {
+      height: 350,
+      type: 'bar',
+      stacked: true,
+      toolbar: {
+        show: true
+      },
+      zoom: {
+        enabled: true
+      }
+    },
+    plotOptions: {
+      bar: {
+        horizontal: true,
+      },
+    },
+    xaxis: {
+      categories: ['Success Rate'],
+    },
+    legend: {
+      position: 'top',
+    },
+    fill: {
+      opacity: 1
+    }
+  };
+
+  var chart = new ApexCharts(document.querySelector("#chart"), options);
+  chart.render();
   return (
     <>       
-            <div className="working-area row bg-color">
+            <div className="row">            
               <div className="col-2 sidebar">
                 <ul className="nav flex-column">
                   <li className="nav-item">
                     <a className="nav-link active" href="#">
-                      <i className="fas fa-tachometer-alt mr-2"></i>Dashboard
+                      <i className="fas fa-home mr-2"></i>Home
                     </a>
                   </li>
                   <li className="nav-item">
                     <a className="nav-link" href="#">
-                      <i className="fas fa-users mr-2"></i>Users
+                      <i className="fas fa-user-graduate mr-2"></i>Student
                     </a>
                   </li>
                   <li className="nav-item">
@@ -20,14 +61,53 @@ function Dashboard() {
                       <i className="fas fa-file-alt mr-2"></i>Reports
                     </a>
                   </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      <i className="fas fa-cog mr-2"></i>Settings
+                    </a>
+                  </li>
                 </ul>
               </div>
 
           
-              <div className="col-10 working-area bg-tertiary">
-                <div className="working-area-content">
-                  <h1>Welcome to the Dashboard</h1>
-                  <p>This is where you can see all the important information at a glance.</p>
+              <div className="col-10 working-area">
+                <div className="working-area-content">                        
+                          <div class="container">
+                          <div className='row'>
+                                <div className='col-12'>
+                                    <div className='card'><div className='card-body bg-dark'>  <h3>Clinical Education Tracking System Dashboard</h3></div></div>          
+                                </div>
+                          </div> 
+                                  <div class="row">
+                                    <div class="col-md-4">
+                                      <div class="card">
+                                        <div class="card-body">
+                                          <h5 class="card-title">Number of Students</h5>
+                                          <p class="card-text text-center"><i class="fas fa-user-graduate text-center fa-3x"></i> </p>
+                                          <canvas id="students-chart">300</canvas>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                      <div class="card">
+                                        <div class="card-body">
+                                          <h5 class="card-title">Placements by Type</h5>
+                                          <p class="card-text text-center"><i class="fas fa-chart-pie  fa-3x"></i></p>
+                                          <canvas id="placements-chart"></canvas>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                      <div class="card">
+                                        <div class="card-body">
+                                          <h5 class="card-title">Success Rate</h5>
+                                          <p class="card-text text-center"><i class="fas fa-chart-bar text-center fa-3x"></i></p>
+                                          <canvas id="success-rate-chart"></canvas>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
                 </div>
               </div>
             </div>
