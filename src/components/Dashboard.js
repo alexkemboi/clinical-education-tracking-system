7859219c74ce2738f418c9f49fec5f36f6b1aaf4
alexkemboi4127 <alexkemboi97@yahoo.com>
2023-05-05 06:09:34 +0300

    added clinical

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,69 +1,99 @@
-import React,{useState} from 'react';
-import ApexCharts from 'apexcharts';
-import './styles/DashboardStyles.css';
-import LoginPage from '../LoginPage';
-import ClinicalManagement from './ClinicalManagement';
-import StudentRegistratiion from './StudentRegistration';
+import React, { useState } from "react";
+import ApexCharts from "apexcharts";
+//import "./styles/DashboardStyles.css";
+import LoginPage from "../LoginPage";
+import ClinicalManagement from "./ClinicalManagement";
+import StudentRegistratiion from "./StudentRegistration";
 
 function Dashboard() {
-  const [showLoginForm,setShowLoginForm]=useState(false);
-  const [showDashboard,setShowDashboard]=useState(true);
-  const [showStudentForm,setShowStudentForm]=useState(false);
-  function handleLogout(){
-      setShowLoginForm(true);
-      setShowDashboard(false);
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showDashboard, setShowDashboard] = useState(true);
+  const [showStudentForm, setShowStudentForm] = useState(false);
+  const [showClinicalManagement, setShowClinicalManagement] = useState(false);
+  function handleLogout() {
+    setShowLoginForm(true);
+    setShowDashboard(false);
   }
-  function handleShowStudentForm(){
+  function handleShowStudentForm() {
     setShowStudentForm(true);
   }
+  function handleClinicalManagement() {
+    setShowClinicalManagement(true);
+    setShowLoginForm(false);
+    setShowStudentForm(false);
+  }
   return (
-    <>     {  showDashboard && <section>
-            <div className="row">            
-              <div className="col-2 sidebar">
-                <ul className="nav flex-column">
-                  <li className="nav-item">
-                    <a className="nav-link active" href="#">
-                      <i className="fas fa-home mr-2"></i>Home
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <button onClick={handleShowStudentForm} className='btn btn-dark'>
-                      <i className="fas fa-user-graduate mr-2"></i>Student 
-                    </button>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      <i className="fas fa-notes-medical mr-2"></i>Clinicals
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      <i className="fas fa-file-alt mr-2"></i>Reports
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">
-                      <i className="fas fa-cog mr-2"></i>Settings
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <button className="btn btn-dark"  onClick={handleLogout}>
-                      <i className="fas fa-sign-out-alt mr-2"></i>Log out 
-                    </button>
-                  </li>
-                </ul>
-              </div>
+    <>
+      {" "}
+      {showDashboard && (
+        <div className="card h-100 w-100">
+          <div className="card-header bg-dark">
+            <div className="row  text-centre ">
+              <h3 className="text-centre m-auto text-light">
+                Clinical Rotation Request Form
+              </h3>
+            </div>
+          </div>
+          <div className="card-body">
+            <section>
+              <div className="row">
+                <div className="col-2 sidebar ml-0 bg-dark text-light h-100 w-25">
+                  <ul className="nav flex-column">
+                    <li className="nav-item">
+                      <a className="nav-link active" href="#">
+                        <i className="fas fa-home mr-2"></i>Home
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        href="#"
+                        onClick={handleShowStudentForm}
+                        className="nav-link"
+                      >
+                        <i className="fas fa-user-graduate mr-2"></i>Student
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a
+                        className="nav-link"
+                        href="#"
+                        onClick={handleClinicalManagement}
+                      >
+                        <i className="fas fa-notes-medical mr-2"></i>Clinicals
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        <i className="fas fa-file-alt mr-2"></i>Reports
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        <i className="fas fa-cog mr-2"></i>Settings
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#" onClick={handleLogout}>
+                        <i className="fas fa-sign-out-alt mr-2"></i>Log out
+                      </a>
+                    </li>
+                  </ul>
+                </div>
 
-          
-              <div className="col-10 working-area">
-                <div className="working-area-content">                        
-                          {showStudentForm && <StudentRegistratiion/>}
+                <div className="col-10 working-area mr-0  bg-light h-100 m-25">
+                  <div className="working-area-content">
+                    {showStudentForm && <StudentRegistratiion />}
+                    {showClinicalManagement && <ClinicalManagement />}
+                  </div>
                 </div>
               </div>
-            </div>
-            </section>}
-         {showLoginForm && <LoginPage/>} 
-  </>
+            </section>
+          </div>
+          <div className="card-footer bg-dark p-10"></div>
+        </div>
+      )}
+      {showLoginForm && <LoginPage />}
+    </>
   );
 }
 
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,67 +1,116 @@
-import React,{ useState } from "react";
-import LoginPage from '../LoginPage';
-function Signup(){
-    const [showLoginForm,setShowLoginForm]=useState(false);
-    const [showSignUpForm,setShowSignUpForm]=useState(true);
-    function handleLogin(){
-        setShowLoginForm(true);
-        setShowSignUpForm(false);
-    }
-       return   ( 
-        <>
-       {showSignUpForm && <form className="container">
-            <div class="card">
-          <div class="card-header bg-dark text-white text-center">
-          <i class="fas fa-user-md fa-3x"></i>
-          <h4 class="mb-0"> Signup Clinical Education Tracking System</h4>
-
-          </div>
-          <div class="card-body">
-          <div class="form-group">
-                    <label for="name"><i class="fas fa-user"></i> Name:</label>
-                    <input type="text" class="form-control" id="name" name="name" required/>
+import React, { useState } from "react";
+import LoginPage from "../LoginPage";
+function Signup() {
+  const [showLoginForm, setShowLoginForm] = useState(false);
+  const [showSignUpForm, setShowSignUpForm] = useState(true);
+  function handleLogin() {
+    setShowLoginForm(true);
+    setShowSignUpForm(false);
+  }
+  return (
+    <>
+      {showSignUpForm && (
+        <div className="row">
+          <div className="col-4 m-auto">
+            <form className="container">
+              <div class="card">
+                <div class="card-header bg-dark text-white text-center">
+                  <i class="fas fa-user-md fa-3x"></i>
+                  <h4 class="mb-0">
+                    {" "}
+                    Signup Clinical Education Tracking System
+                  </h4>
                 </div>
-                <div class="form-group">
-                    <label for="email"><i class="fas fa-envelope"></i> Email:</label>
-                    <input type="email" class="form-control" id="email" name="email" required/>
-                </div>
-                <div class="form-group">
-                    <label for="password"><i class="fas fa-lock"></i> Password:</label>
-                    <input type="password" class="form-control" id="password" name="password" required/>
-                </div>
-                <div class="form-group">
-                    <label for="confirm-password"><i class="fas fa-lock"></i> Confirm Password:</label>
-                    <input type="password" class="form-control" id="confirm-password" name="confirm-password" required/>
-                </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="terms" name="terms" required/>
-                    <label class="form-check-label" for="terms">I agree to the <a href="#">Terms and Conditions</a>.</label>
-                </div>
-                <button type="submit" class="btn btn-dark">Sign Up</button>
-                <div>
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="name">
+                      <i class="fas fa-user"></i> Name:
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="name"
+                      name="name"
+                      required
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="email">
+                      <i class="fas fa-envelope"></i> Email:
+                    </label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="email"
+                      name="email"
+                      required
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="password">
+                      <i class="fas fa-lock"></i> Password:
+                    </label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="password"
+                      name="password"
+                      required
+                    />
+                  </div>
+                  <div class="form-group">
+                    <label for="confirm-password">
+                      <i class="fas fa-lock"></i> Confirm Password:
+                    </label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="confirm-password"
+                      name="confirm-password"
+                      required
+                    />
+                  </div>
+                  <div class="form-group form-check">
+                    <input
+                      type="checkbox"
+                      class="form-check-input"
+                      id="terms"
+                      name="terms"
+                      required
+                    />
+                    <label class="form-check-label" for="terms">
+                      I agree to the <a href="#">Terms and Conditions</a>.
+                    </label>
+                  </div>
+                  <button type="submit" class="btn btn-dark">
+                    Sign Up
+                  </button>
+                  <div>
                     <div className="row mt-4">
-                        <div className="col-8">
-                              <p className="text-danger">Already registered?</p>
-                        </div>
-                        <div className="col-4">
-                        <button  className="btn btn-warning" onClick={handleLogin} >Back to login</button>
-                        </div>
+                      <div className="col-8">
+                        <p className="text-danger">Already registered?</p>
+                      </div>
+                      <div className="col-4">
+                        <button
+                          className="btn btn-warning"
+                          onClick={handleLogin}
+                        >
+                          Back to login
+                        </button>
+                      </div>
                     </div>
-               
+                  </div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      )}
+      {showLoginForm && <LoginPage />}
+    </>
+  );
+}
+{
+}
 
-                </div>
-                </div>
-                </div>              
-        </form>
-            }
-        {showLoginForm && <LoginPage/>}
-        </>      
-        
-       
-      )
-    }
-    {
-       
-    }
-        
 export default Signup;
